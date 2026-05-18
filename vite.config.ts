7d8e67596348eb00bed321daf306bbcb3d9559cb
Abhishek Kumar <abhishekkumar912800@gmail.com>
2026-05-18 23:34:02 +0530

    Fixed PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -43,13 +43,13 @@ export default defineConfig({
 						src: '/192.png',
 						sizes: '192x192',
 						type: 'image/png',
-						purpose: 'any maskable'
+						purpose: 'any'
 					},
 					{
 						src: '/516.png',
 						sizes: '512x512',
 						type: 'image/png',
-						purpose: 'any maskable'
+						purpose: 'any'
 					}
 				],
 
