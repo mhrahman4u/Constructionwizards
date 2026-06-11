b6acaee5e67e708c08f768981b0ce0a450edd7d6
Abhishek Kumar <abhishekkumar912800@gmail.com>
2026-06-11 18:22:03 +0530

    Fixed PWA manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -282,7 +282,7 @@ h1{
   import { supabase } from "$lib/supabase";
   import { snackbar } from "$lib/snackbar";
 
-  const superAdminUid = "Zzjym4rd6jbxlDsUxCMxZbx2juu2";
+  const superAdminUid = "SlEcCRpD6NSPIQDwK46TWhTXzn22";
 
   let loading = true;
 
